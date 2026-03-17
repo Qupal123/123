8272a85a67bd3d306fcf9a2b43700bdf727dda62
Qupal123 <grt_qupal@mail.ru>
2026-03-17 20:49:10 +0600

    Add main App component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import CharacterGallery from './components/CharacterGallery'
+import Stats from './components/Stats'
+import Footer from './components/Footer'
+import './styles/App.css'
+
+export default function App() {
+  const [selectedBrawler, setSelectedBrawler] = useState(null)
+
+  return (
+    <div className="app">
+      <Header />
+      <Hero />
+      <CharacterGallery onSelectBrawler={setSelectedBrawler} />
+      {selectedBrawler && <Stats brawler={selectedBrawler} onClose={() => setSelectedBrawler(null)} />}
+      <Footer />
+    </div>
+  )
+}
